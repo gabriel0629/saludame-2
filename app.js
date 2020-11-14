b40b71597b1330b576a6921f1b0bf29a260dc15b
Gabriel <gabrielacevedo.0629@gmail.com>
2020-11-14 18:46:04 -0500

    reducción espacio en el string Hola Juan!

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/makers/:nombre", (req, res) => {    
     const nombre = req.params.nombre;
-    res.send(`<h1>Hola ${nombre.charAt(0).toUpperCase()+nombre.slice(1)}! `);         
+    res.send(`<h1>Hola ${nombre.charAt(0).toUpperCase()+nombre.slice(1)}!`);         
 });
 
 app.listen(3000, () => console.log('Listening on port 3000'));
